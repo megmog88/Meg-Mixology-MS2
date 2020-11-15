517d7ce7168e9076b5f9225a098b83fc88e01fed
megmog88 <meghanspence88@hotmail.co.uk>
2020-11-15 14:29:10 +0000

    added javascript anf buttons to try and search by ingredient, however keep running into an error, removed confetti

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,79 +4,24 @@ $(document).ready(function(){
 $('.myModal').modal('show'); 
 
 });
+ 
+//------Function to search cocktails by ingredient
 
-//------Confetti For Header(Taken from a post by Saijo George and editted)
-const canvasEl = document.querySelector('#canvas');
+document.getElementById('searchGin').addEventListener('click', searchGin);
 
-const w = canvasEl.width = window.innerWidth;
-const h = canvasEl.height = window.innerHeight * 2;
-
-function loop() {
-  requestAnimationFrame(loop);
-	ctx.clearRect(0,0,w,h);
-  
-  confs.forEach((conf) => {
-    conf.update();
-    conf.draw();
-  })
-}
-
-function Confetti () {
-  //construct confetti
-  const colours = ['#eccb7a'];
-  
-  this.x = Math.round(Math.random() * w);
-  this.y = Math.round(Math.random() * h)-(h/2);
-  this.rotation = Math.random()*360;
-
-  const size = Math.random()*(w/60);
-  this.size = size < 15 ? 15 : size;
-
-  this.color = colours[Math.floor(colours.length * Math.random())];
-
-  this.speed = this.size/7;
-  
-  this.opacity = Math.random();
-
-  this.shiftDirection = Math.random() > 0.5 ? 1 : -1;
-}
-
-Confetti.prototype.border = function() {
-  if (this.y >= h) {
-    this.y = h;
-  }
-}
-
-Confetti.prototype.update = function() {
-  this.y += this.speed;
-  
-  if (this.y <= h) {
-    this.x += this.shiftDirection/3;
-    this.rotation += this.shiftDirection*this.speed/100;
-  }
-
-  if (this.y > h) this.border();
-};
-
-Confetti.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.arc(this.x, this.y, this.size, this.rotation, this.rotation+(Math.PI/2));
-  ctx.lineTo(this.x, this.y);
-  ctx.closePath();
-  ctx.globalAlpha = this.opacity;
-  ctx.fillStyle = this.color;
-  ctx.fill();
-};
-
-const ctx = canvasEl.getContext('2d');
-const confNum = Math.floor(w / 4);
-const confs = new Array(confNum).fill().map(_ => new Confetti());
-
-loop();
-
-//------Function to Get a List of Popular Cocktails
-function getPopularCocktails(){
-fetch('https://www.thecocktaildb.com/api/json/v2/9973533/popular.php')
-    .then(response => response.json())
-    .then(data => console.log(data));
+function searchGin(){
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin')
+  .then(response => response.json())
+  .then(data => {
+      let output = '<h2>Cocktails</h2>';
+          data.forEach(function(drinks) {
+          output += `
+          <div>
+            <h3>${drinks.strDrink}</h3> 
+            <img src="${drinks.strDrinkThumb} alt="drink thumbnail">
+          </div>`
+          ;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
 }
