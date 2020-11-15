19152c18ecc6c2b59d4b482de0907e5cd807c4c7
megmog88 <meghanspence88@hotmail.co.uk>
2020-11-15 15:55:12 +0000

    fixed error with forEach, now search works for gin button. To be added for all other buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,17 +11,20 @@ document.getElementById('searchGin').addEventListener('click', searchGin);
 
 function searchGin(){
     fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin')
-  .then(response => response.json())
+  .then(response => response.json()) 
   .then(data => {
+      const { drinks } = data;
       let output = '<h2>Cocktails</h2>';
-          data.forEach(function(drinks) {
-          output += `
+      drinks.forEach(drink => {
+   // console.log(drink);
+     output += `
           <div>
-            <h3>${drinks.strDrink}</h3> 
-            <img src="${drinks.strDrinkThumb} alt="drink thumbnail">
-          </div>`
-          ;
-      });
+            <h3>${drink.strDrink}</h3> 
+            <img src="${drink.strDrinkThumb}" alt="drink thumbnail"/>
+          </div>`;
+        });
+         
+    console.log(output)
       document.getElementById('output').innerHTML = output;
     })
 }
